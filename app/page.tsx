2b60a79a9bd4c0c1fd9a57b585a3cdd57a9b1c85
bobi.rival <bahmadrival@gmail.com>
2025-03-03 11:27:38 +0700

    Add meaning to kanji

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
     const [answerStatus, setAnswerStatus] = useState<AnswerStatus>(startingAnswerStatus());
     const [nextQuestion, setNextQuestion] = useState<number>(0);
     const [isModalSelectLevelOpen, setIsModalSelectLevelOpen] = useState<boolean>(false);
+    const [isKanjiToMeaning, setIsKanjiToMeaning] = useState<boolean>(true);
 
     useEffect((): void => {
         const dataKotobaList = getSelectLevelMap().get(currentLevel) ?? [];
@@ -158,13 +159,27 @@ export default function Home() {
     return (
         <section className="bg-gray-100 h-screen">
             <div className="lg:flex lg:justify-between bg-gray-200 border-gray-500 font-light">
-                <div></div>
-                <div className="flex flex-col lg:flex-row gap-1 lg:gap-4">
-                    <div className="lg:py-2 flex justify-end pt-1 pr-1">
+                <div className="flex justify-between w-full">
+                    <div className="lg:py-2 py-1 pl-1">
+                        <button onClick={() => setIsKanjiToMeaning(!isKanjiToMeaning)} className="bg-gray-500 hover:bg-gray-400 cursor-pointer px-5 py-1 text-white rounded flex justify-center items-center gap-2">
+                            {isKanjiToMeaning ? (
+                                <>
+                                    Arti <i className="fa-solid fa-arrow-right text-white text-sm" /> Kanji
+                                </>
+                            ) : (
+                                <>
+                                    Kanji <i className="fa-solid fa-arrow-right text-white text-sm" /> Arti
+                                </>
+                            )}
+                        </button>
+                    </div>
+                    <div className="lg:py-2 py-1 pr-1">
                         <button onClick={() => setIsModalSelectLevelOpen(!isModalSelectLevelOpen)} className="bg-gray-500 hover:bg-gray-400 cursor-pointer px-5 py-1 text-white rounded">
                             Pilih Level
                         </button>
                     </div>
+                </div>
+                <div className="flex flex-col lg:flex-row gap-1 lg:gap-4">
                     <div className="bg-gray-300 flex justify-between items-center gap-4 px-10 py-2 lg:py-0">
                         <h1>Benar : {answerStatus.correct}</h1>
                         <h1>Salah : {answerStatus.incorrect}</h1>
@@ -183,7 +198,14 @@ export default function Home() {
                             </div>
                         </>
                     )}
-                    <p className="text-[30px] lg:text-[50px] text-center">{question?.kanji}</p>
+                    {isKanjiToMeaning ? (
+                        <p className="text-[30px] lg:text-[50px] text-center">{question?.kanji}</p>
+                    ) : (
+                        <div className="p-2">
+                            <p className="text-[30px] lg:text-[50px] text-center capitalize">{question?.meaning}</p>
+                            <p className="text-sm lg:text-base text-center">{question?.hiragana}</p>
+                        </div>
+                    )}
                 </div>
                 <div className="w-[350px] h-[350px] lg:w-[450px] lg:h-[450px] grid grid-cols-3 gap-2 lg:gap-3 justify-self-center lg:justify-self-start">
                     {answers.map((answer) => (
@@ -197,8 +219,14 @@ export default function Home() {
                                 </>
                             )}
                             <div className="w-[108px] h-[108px] lg:w-[140px] lg:h-[140px] p-2 flex flex-col justify-center items-center">
-                                <p className="text-center font-light text-gray-900 capitalize text-xs lg:text-sm leading-4">{answer.meaning}</p>
-                                <p className="text-center font-light text-gray-700 text-[10px] lg:text-xs mt-1">{answer.hiragana}</p>
+                                {isKanjiToMeaning ? (
+                                    <div>
+                                        <p className="text-center font-light text-gray-900 capitalize text-xs lg:text-sm leading-4">{answer.meaning}</p>
+                                        <p className="text-center font-light text-gray-700 text-[10px] lg:text-xs mt-1">{answer.hiragana}</p>
+                                    </div>
+                                ) : (
+                                    <p className="text-center font-light text-gray-900 capitalize text-xs lg:text-base leading-4">{answer.kanji}</p>
+                                )}
                             </div>
                         </button>
                     ))}
