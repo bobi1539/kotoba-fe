7f9d7bf0ce620af4d00d375ae6ed1bda70a4a1b9
bobi.rival <bahmadrival@gmail.com>
2025-05-01 14:04:05 +0700

    Create page table

--- app/(page)/kotoba-table/page.tsx
+++ app/(page)/kotoba-table/page.tsx
@@ -1,3 +1,35 @@
+"use client";
+
+import { useKotobaContext } from "@/app/context/kotoba-context";
+import { getSelectLevelMap } from "@/app/util/helper";
+
 export default function KotobaTable() {
-    return <h1>Hello</h1>
+    const { currentLevel } = useKotobaContext();
+    const dataKotobaList = getSelectLevelMap().get(currentLevel) ?? [];
+
+    return (
+        <div className="overflow-x-auto m-2 lg:mx-10 lg:my-5">
+            <table className="w-full text-left text-gray-600 text-xs lg:text-xl">
+                <thead className="text-black uppercase bg-gray-100">
+                    <tr>
+                        <th className="text-center p-2">No</th>
+                        <th>Kanji</th>
+                        <th>Hiragana</th>
+                        <th>Arti</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dataKotobaList.map((kotoba, index) => (
+                        <tr key={kotoba.id} className="border-b border-gray-300">
+                            <td className="text-center p-1">{index + 1}</td>
+                            <td>{kotoba.kanji}</td>
+                            <td>{kotoba.hiragana}</td>
+                            <td>{kotoba.meaning}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className="h-20"/>
+        </div>
+    );
 }
--- app/component/navbar/navbar.tsx
+++ app/component/navbar/navbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { ENDPOINT_KOTOBA_TABLE, ENDPOINT_PRACTICE } from "@/app/constant/endpoint";
 import { useKotobaContext } from "@/app/context/kotoba-context";
+import { useRouter } from "next/navigation";
 
 interface NavbarProps {
     isModalSelectLevelOpen: boolean;
@@ -9,26 +11,38 @@ interface NavbarProps {
 
 export default function Navbar(props: Readonly<NavbarProps>) {
     const { isKanjiToMeaning, setIsKanjiToMeaning, answerStatus } = useKotobaContext();
+    const router = useRouter();
 
     return (
         <div className="lg:flex lg:justify-between bg-gray-200 border-gray-500 font-light">
             <div className="flex justify-between w-full">
-                <div className="lg:py-2 py-1 pl-1">
-                    <button onClick={() => setIsKanjiToMeaning(!isKanjiToMeaning)} className="bg-gray-500 hover:bg-gray-400 cursor-pointer px-5 py-1 text-white rounded flex justify-center items-center gap-2">
-                        {isKanjiToMeaning ? (
-                            <>
-                                Arti <i className="fa-solid fa-arrow-right text-white text-sm" /> Kanji
-                            </>
-                        ) : (
-                            <>
-                                Kanji <i className="fa-solid fa-arrow-right text-white text-sm" /> Arti
-                            </>
-                        )}
-                    </button>
+                <div className="flex">
+                    <div className="lg:py-2 py-1 pl-1">
+                        <button onClick={() => setIsKanjiToMeaning(!isKanjiToMeaning)} className="bg-gray-500 hover:bg-gray-400 cursor-pointer px-5 py-1 text-white rounded flex justify-center items-center gap-2">
+                            {isKanjiToMeaning ? (
+                                <>
+                                    Arti <i className="fa-solid fa-arrow-right text-white text-sm" /> Kanji
+                                </>
+                            ) : (
+                                <>
+                                    Kanji <i className="fa-solid fa-arrow-right text-white text-sm" /> Arti
+                                </>
+                            )}
+                        </button>
+                    </div>
+                    <div className="lg:py-2 py-1 pl-1 flex gap-1">
+                        <button onClick={() => router.push(ENDPOINT_PRACTICE)} className="bg-gray-500 hover:bg-gray-400 cursor-pointer px-5 py-1 text-white rounded flex justify-center items-center gap-2">
+                            Latihan
+                        </button>
+                        <button onClick={() => router.push(ENDPOINT_KOTOBA_TABLE)} className="bg-gray-500 hover:bg-gray-400 cursor-pointer px-5 py-1 text-white rounded flex justify-center items-center gap-2">
+                            Tabel
+                        </button>
+                    </div>
                 </div>
+
                 <div className="lg:py-2 py-1 pr-1">
                     <button onClick={() => props.setIsModalSelectLevelOpen(!props.isModalSelectLevelOpen)} className="bg-gray-500 hover:bg-gray-400 cursor-pointer px-5 py-1 text-white rounded">
-                        Pilih Level
+                        Level
                     </button>
                 </div>
             </div>
